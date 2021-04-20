c52ae174b5743a8e3191b6e738cbe837afb8ac55
selenslmn <56594127+selenslmn@users.noreply.github.com>
2021-04-21 00:58:47 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,33 @@
-#Explain your work
+# Homework 1
 
 #Question 1
-for x in range(a):
-	print(a)
+"İstatistiksel işlemler ile veriler üzerinden çıkarımlar yaparak tahminlerde bulunan sistemlerin bilgisayarlar ile modellenmesidir."
+
+#Question 2
+"Denetimli Öğrenmede etiketlenmiş veriler kullanılır, eğitim verisi hem girdilerden hem de çıktılardan oluşur, veri setindeki çıkışlar kategorik ise sınıflandırma nümerik ise regresyon algoritmaları kullanılır."
+"Denetimsiz Öğrenmede ise etiketlenmemiş veriler üzerinden bilinmeyen bir yapıyı tahmin etmek için bir algoritma kullanan makine öğrenmesi tekniğidir, burada giriş verisinin hangi sınıfa ait olduğu belirsizdir."
+"Denetimli Öğrenme algoritmalarına k-en yakın komşu, karar ağaçları ve lojistik regresyonu örnek verebiliriz."
+"K-en yakın komşu: k bir veri kümesindeki sonuçları sınıflandırmak veya tahmin etmek için kullanılan en yakın komşuların sayısını ifade eder. \n" 
+"Her yeni gözlemin sınıflandırması veya tahmini, ağırlıklı ortalamalara göre belirlenen bir mesafeye (yani En Yakın Komşu) göre hesaplanır."
+"Karar Ağaçları: Bir karar ağacı, çok sayıda kayıt içeren bir veri kümesini, bir dizi karar kuralları uygulayarak daha küçük kümelere bölmek için kullanılan bir yapıdır. \n"
+"Yani basit karar verme adımları uygulanarak, büyük miktarlardaki kayıtları, çok küçük kayıt gruplarına bölerek kullanılan bir yapıdır."
+"Lojistik regresyon: Sonuç, ikili bir değişkenle ölçülür (1-0, yalnızca iki olası sonuç vardır). \n"
+"Bağımlı değişken ile ilgili bir dizi bağımsız değişken arasındaki ilişkiyi tanımlamak için en uygun modeli bulmaktır."
+"Denetimsiz Öğrenme algoritmalarına K-kümeleme, Hiyerarşik kümeleme ve kohonen ağları örnek verebiliriz."
+"K-kümeleme:
+"Hiyerarşik kümeleme:
+"kohonen ağları:
+
+#Question 3
+"
+
+#Question 4
+"
+
+#Question 5
+"
+
+#Question 6
+"
+
+#Question 7
